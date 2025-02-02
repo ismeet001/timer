5808fa69124060abc35c44d32c13354d446e5949
ismeet010 <ismeet.k.chhabda@gmail.com>
2025-02-02 21:56:54 +0530

    feat: Use localStorage to save timers

--- src/store/useTimerStore.ts
+++ src/store/useTimerStore.ts
@@ -2,8 +2,17 @@ import { configureStore, createSlice } from '@reduxjs/toolkit';
 import { useDispatch, useSelector } from 'react-redux';
 import { Timer } from '../types/timer';
 
+const loadTimersFromLocalStorage = () => {
+  const timers = localStorage.getItem('timers');
+  return timers ? JSON.parse(timers) : [];
+};
+
+const saveTimersToLocalStorage = (timers: Timer[]) => {
+  localStorage.setItem('timers', JSON.stringify(timers));
+};
+
 const initialState = {
-  timers: [] as Timer[],
+  timers: loadTimersFromLocalStorage() as Timer[],
 };
 
 const timerSlice = createSlice({
@@ -11,20 +20,24 @@ const timerSlice = createSlice({
   initialState,
   reducers: {
     addTimer: (state, action) => {
-      state.timers.push({
+      const newTimer = {
         ...action.payload,
         id: crypto.randomUUID(),
         createdAt: Date.now(),
-      });
+      };
+      state.timers.push(newTimer);
+      saveTimersToLocalStorage(state.timers); 
     },
     deleteTimer: (state, action) => {
       state.timers = state.timers.filter(timer => timer.id !== action.payload);
+      saveTimersToLocalStorage(state.timers); 
     },
     toggleTimer: (state, action) => {
-      const timer = state.timers.find(timer => timer.id === action.payload);
+     const timer = state.timers.find(timer => timer.id === action.payload);
       if (timer) {
         timer.isRunning = !timer.isRunning;
       }
+      saveTimersToLocalStorage(state.timers); 
     },
     updateTimer: (state, action) => {
       const timer = state.timers.find(timer => timer.id === action.payload);
@@ -32,6 +45,7 @@ const timerSlice = createSlice({
         timer.remainingTime -= 1;
         timer.isRunning = timer.remainingTime > 0;
       }
+      saveTimersToLocalStorage(state.timers); 
     },
     restartTimer: (state, action) => {
       const timer = state.timers.find(timer => timer.id === action.payload);
@@ -39,6 +53,7 @@ const timerSlice = createSlice({
         timer.remainingTime = timer.duration;
         timer.isRunning = false;
       }
+      saveTimersToLocalStorage(state.timers); 
     },
     editTimer: (state, action) => {
       const timer = state.timers.find(timer => timer.id === action.payload.id);
@@ -47,6 +62,7 @@ const timerSlice = createSlice({
         timer.remainingTime = action.payload.updates.duration || timer.duration;
         timer.isRunning = false;
       }
+      saveTimersToLocalStorage(state.timers); 
     },
   },
 });
